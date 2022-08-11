1c1aac9d7b58cff6848ea44f9bb2a8c26d8235df
tkornilova <kornilova.tat07@gmail.com>
2022-08-11 11:15:45 +0400

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.result = Math.ceil((this.max + this.min) / 2);
     }
 
     lower() {
-
+        this.max = this.result;
+        this.guess();
     }
 
     greater() {
-
+        this.min = this.result;
+        this.guess();
     }
 }
 
